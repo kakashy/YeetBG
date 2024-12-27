7aaa81194e39b5abd175eb7e2edbab72c8094c6c
Joseph Waweru <josephwawerug@gmail.com>
2024-12-27 10:13:18 +0300

    chore: support for dark mode toggle

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-	import { ModeWatcher } from 'mode-watcher';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { Button } from '$lib/components/ui/button/index.js';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<Button on:click={toggleMode} variant="outline" class="fixed right-0 top-0" size="icon">
+	<Sun
+		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+	/>
+	<Moon
+		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
 
 <ModeWatcher />
 <Toaster />
